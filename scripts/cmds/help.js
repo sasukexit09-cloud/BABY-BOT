79517cd55008ab734111102474994159ce0d0e03
farhuu0 <mstlimak672@gmail.com>
2025-06-21 22:52:17 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -64,7 +64,7 @@ module.exports = {
       msg += ``;
       msg += `\n╭─────✰\n│ ╣[𝗙 𝗔 𝗥 𝗛 𝗔 𝗡]╠\n╰────────────✰`; 
 
-const helpListImages = [ "https://files.catbox.moe/3q8ubu.jpg" ];
+const helpListImages = [ "https://files.catbox.moe/148z2x.jpg" ];
 
 
       const helpListImage = helpListImages[Math.floor(Math.random() * helpListImages.length)];
