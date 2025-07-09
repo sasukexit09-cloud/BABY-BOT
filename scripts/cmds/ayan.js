732bfcee85086bec3ecb14fd6f085d746aba9ec1
farhuu0 <mstlimak672@gmail.com>
2025-07-09 12:28:39 +0600

    Update farhan.js

--- scripts/cmds/ayan.js
+++ scripts/cmds/ayan.js
@@ -2,7 +2,7 @@ module.exports = {
 config: {
 name: "farhan",
 version: "1.0",
-author: "aYan",
+author: "Farhan",
 countDown: 5,
 role: 0,
 shortDescription: "no prefix",
@@ -14,7 +14,7 @@ onStart: async function(){},
 onChat: async function({ event, message, getLang }) {
 if (event.body && event.body.toLowerCase() === "farhan") {
 return message.reply({
-body: " ──────────◊\n‣ 𝐁𝐨𝐭 & 𝐎𝐰𝐧𝐞𝐫 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧  \n\n‣ 𝐍𝐚𝐦𝐞: FarHan                           ‣ 𝐁𝐨𝐭 𝐍𝐚𝐦𝐞:🕸️ SpideY 🕷️ 」",
+body: " ╭────────────◊\n├‣ 𝐎𝐰𝐧𝐞𝐫 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧  \n├───────────◊ \n├‣ 𝐍𝐚𝐦𝐞 : Fᴀʀʜᴀɴ ʜᴀsᴀɴ ᴊɪʙᴏɴ  \n├‣ 𝐇𝐨𝐦𝐞 : Sɪʀᴀᴊɢᴀɴᴊ \n├‣ 𝐂𝐥𝐚𝐬𝐬 : Ssᴄ ᴅᴏɴᴇ \n╰───────────◊  ",
 attachment: await global.utils.getStreamFromURL("https://files.catbox.moe/87d2eo.mp4")
 });
 }
