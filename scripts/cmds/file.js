e91c1754b3dc3ab58ed6289d8006b184d118ec3f
𝐀𝐅𝐑𝐎𝐙𝐀 𝐁𝐀𝐁𝐘 <sasukexit09@gmail.com>
2025-09-27 13:34:35 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100080736881604",];
+		const permission = ["61573375301770",];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" You don't have permission to use this command. 🐤", event.threadID, event.messageID);
 		}
