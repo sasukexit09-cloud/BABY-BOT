46adf13418cc94f87466b9d011dcdca1e8c88f51
𝐀𝐅𝐑𝐎𝐙𝐀 𝐁𝐀𝐁𝐘 <sasukexit09@gmail.com>
2025-10-16 15:58:54 +0600

    Update attachment URL in prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -118,7 +118,7 @@ module.exports = {
         + `║ 💬 Chatbox : ${threadPrefix}\n`
         + `║ ➤ ${threadPrefix}help to see all available cmds 🥵\n`
         + "╚═══════════════╝",
-        attachment: await utils.getStreamFromURL("https://drive.google.com/uc?export=download&id=1xljsQQl2cp1PKW5jyhsnWabxuQ49RMXl")
+        attachment: await utils.getStreamFromURL("https://files.catbox.moe/5tmm3l.jpg")
       });
     }
   }
