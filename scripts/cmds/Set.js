17d4b7db65e6586f89e921eaa5b3e51c45cbb98f
𝐀𝐅𝐑𝐎𝐙𝐀 𝐁𝐀𝐁𝐘 <sasukexit09@gmail.com>
2025-10-16 17:09:42 +0600

    Update Set.js

--- scripts/cmds/Set.js
+++ scripts/cmds/Set.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   onStart: async function ({ args, event, api, usersData }) {
-    const permission = ["61573375301770"];
+    const permission = ["61582522732634"];
   if (!permission.includes(event.senderID)) {
     api.sendMessage("You don't have enough permission to use this command. Only My Lord Can Use It.", event.threadID, event.messageID);
     return;
